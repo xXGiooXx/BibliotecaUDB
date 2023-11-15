54e252da1f0a62b96a2855b591590a46cdb05e38
JONATHAN G <JONATHAN G@DESKTOP-P6MEKCE>
2023-11-14 22:20:40 -0600

    SE CREO LA CLASE CdDAO

--- ProyectoPoo/src/Controlador/ControladorMenu.java
+++ ProyectoPoo/src/Controlador/ControladorMenu.java
@@ -7,6 +7,7 @@ package Controlador;
 
 import Modelo.Usuario;
 import Modelo.Validacion_login;
+import Vista.CdForm;
 import Vista.Login;
 import Vista.Menu;
 import Vista.UsuarioForm;
@@ -22,19 +23,23 @@ import javax.swing.JOptionPane;
 public class ControladorMenu implements ActionListener {
 
     UsuarioForm usuarioForm;
+    CdForm cdForm;
 
     Login vistaLogin = new Login();
     Validacion_login modeloLogin = new Validacion_login();
 
     public ControladorMenu() {
         usuarioForm = new UsuarioForm(); //SE INSTANCIA Y SE INICIALIZA EL FORMULARIO PARA MANIPULAS LOS EVENTOS Y ACCIONES
+        cdForm = new CdForm();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("Gestionar_Usuarios")) {
             usuarioForm.setVisible(true); //SE MUESTRA LA VENTANA USUARIOFORM AL DAR CLICK EN GESTIONAR USUARIOS
-
+        }
+         if (e.getActionCommand().equals("CDS")) {
+            cdForm.setVisible(true); //SE MUESTRA LA VENTANA CD FORM AL DAR CLICK EN CDS
         }
         if (e.getActionCommand().equals("Salir")) {
             System.exit(0);
